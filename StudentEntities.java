07b1fd59a1f9f99cac2c3524cc202849858130f1
JayaChandra789 <bagamjaya789@gmail.com>
2025-01-14 09:50:27 +0530

    StudentEntities.java

--- StudentEntities.java
+++ StudentEntities.java
@@ -1,7 +1,19 @@
 package com.mystudent.hibernate.student.student_hibernate.entities;
+
+import com.mystudent.hibernate.student.student_hibernate.coursefk.CourseDetailsEntities; //Imported
+
 import jakarta.persistence.*;
+
 /*------Using  @Entity Annotation to declare java class as an entity------ */
 @Entity
+/*-----------------------------------------Named Queries---------------------------------------*/
+@NamedQueries(
+		{
+			@NamedQuery(name="fetchStudentList",query = "from StudentEntities"), //HQL always deals with entities so use class name not table name(StudentEntities)
+			@NamedQuery(name="fetchStudentNames",query="select stdName from StudentEntities"),
+			@NamedQuery(name = "UpdateStandardAndRollAndAge", query = "update StudentEntities set stdStandard = :x, stdRoll = :y, stdAge = :z where stdId = :m")
+		})
+/*IN @NamedQueries({@NamedQuery(name="",query())})*/
 /*-----Using @Table Annotation to set table related information----*/
 @Table(name = "Student_info")
 
@@ -26,8 +38,13 @@ public class StudentEntities
 	@Column(length = 300, nullable = false)
 	private String stdAddress;
 	
+  //  @JoinColumn(name = "course_id") // Ensure column names match database schema
+	@OneToOne 
+	//Plays crucial role if u add it here Insert program wont works until it matches parameters of both classes
+	private CourseDetailsEntities courseData;
+	
 	/*-------------------------Parameterized constructor--------------------------------*/
-	public StudentEntities(String stdId,String stdName, int stdAge,int stdRoll, String stdStandard,String stdAddress) 
+	public StudentEntities(String stdId,String stdName, int stdAge,int stdRoll, String stdStandard,String stdAddress, CourseDetailsEntities courseData) 
 	{
 		super();
 		this.stdId = stdId;
@@ -36,6 +53,8 @@ public class StudentEntities
 		this.stdRoll = stdRoll;
 		this.stdStandard = stdStandard;
 		this.stdAddress = stdAddress;
+		/*Creating for course details as class course acts as data type here and a object for it.*/
+		this.courseData = courseData;
 	}
 	/*-----------------------------------------------------------------*/
 	public StudentEntities() 
@@ -61,6 +80,10 @@ public class StudentEntities
 	
 	public String getStdAddress() {return stdAddress;}
 	public void setStdAddress(String address) {this.stdAddress = address;}
+	/*Creating getter and setter for Course Details Entity class also*/
+	public CourseDetailsEntities getCoursedata() {return courseData;}
+	public void setCoursedata(CourseDetailsEntities coursedata) {this.courseData = coursedata;}
+	
 	/*------------------------------To override and convert all data into strings-----------------------------------*/
 	@Override
 	public String toString() 
@@ -71,6 +94,7 @@ public class StudentEntities
 				"\n Roll Number : " + stdRoll+
 				"\n Standard=" + stdStandard + 
 				"\n Address : " + stdAddress +
+				"\n Course Details: " +courseData +
 				"\n-------------------------------------------------";
 	}		
 }
